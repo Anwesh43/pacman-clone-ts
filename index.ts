ed1a5f93240ba2c231832810983e24cb22aeedd7
Anwesh43 <anweshthecool0@gmail.com>
2021-03-07 17:23:05 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const delay : number = 40 
+const PAC_MAN_COLOR : string = "orange"
+const BACKGROUND_COLOR : string = "#bdbdbd"
+const SPEED : number = 10 
+const deg : number = 60 
+
