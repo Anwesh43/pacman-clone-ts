c48a92ffbed403fbc13c8351c4334d4da1152fde
Anwesh43 <anweshthecool0@gmail.com>
2021-03-07 19:40:27 +0530

    adding Loop class

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
-const delay : number = 40 
+const delay : number = 1000
 const PAC_MAN_COLOR : string = "orange"
 const BACKGROUND_COLOR : string = "#bdbdbd"
 const SPEED : number = 10 
@@ -13,6 +13,7 @@ class Stage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D 
     renderer : Renderer = new Renderer()
+    loop : Loop = new Loop()
 
 
     initCanvas() {
@@ -34,10 +35,17 @@ class Stage {
         }
     }
 
+    start() {
+        this.loop.start(() => {
+            this.render()
+        })
+    }
+
     static init() {
         const stage : Stage = new Stage()
         stage.initCanvas()
         stage.render()
+        stage.start()
         stage.handleKeyDown()
     }
     
@@ -46,16 +54,39 @@ class Stage {
 class Renderer {
 
     k : number = 0 
+
     render(context : CanvasRenderingContext2D) {
         context.font = context.font.replace(/\d+/g, '100')
         const text : string = `Hello:${this.k}`
         context.fillStyle = 'green'
         const {width : tw} = context.measureText(text) //{width : } ->> tw 
         context.fillText(text, w / 2 - tw / 2, h / 2)
+        this.k++;
     }
 
     handleKey(cb : Function) {
 
+    }
+
+}
+
+class Loop {
+    
+    interval : number 
+    running : boolean 
+
+    start(cb : Function) {
+        if (!this.running) {
+            this.running = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.running) {
+            this.running = false 
+            clearInterval(this.interval)
+        }
     }
 }
 
