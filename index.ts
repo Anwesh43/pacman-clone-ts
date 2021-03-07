6ebf2d5aa9c85f3d51b4b9844920a880306eb026
Anwesh43 <anweshthecool0@gmail.com>
2021-03-07 18:08:45 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -6,3 +6,41 @@ const BACKGROUND_COLOR : string = "#bdbdbd"
 const SPEED : number = 10 
 const deg : number = 60 
 
+
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = BACKGROUND_COLOR 
+        this.context.fillRect(0, 0, w, h)
+        this.context.fillStyle = 'green'
+        this.context.fillText("Hello World", w / 2, h / 2)
+    }
+
+    handleKeyDown() {
+        window.onkeydown = (e) => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleKeyDown()
+    }
+    
+}
+
+Stage.init()
